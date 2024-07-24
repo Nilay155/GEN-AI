c71cf3a71c2ac40f9a0d1154589574a686264d1e
Nilay155 <nilayjainofficial2004@gmail.com>
2024-07-24 15:11:22 +0530

    Server Side Setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+app.use(morgan("dev"))
+
+export default app
